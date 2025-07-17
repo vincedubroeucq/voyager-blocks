dc583d50382037ad0a606350a992e5bf63b64cd5
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2025-07-17 16:41:42 +0200

    3-7 - Added single.html template

---
+++ patterns/single-hero.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Single hero
+ * Slug: voyager/single-hero
+ * Categories: Bannières
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":70,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":350,"className":"is-style-dynamic-height","style":{"elements":{"link":{"color":{"text":"var:preset|color|grey-1"}}},"layout":{"selfStretch":"fixed","flexSize":"56vw"}},"textColor":"grey-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-style-dynamic-height has-grey-1-color has-text-color has-link-color" style="min-height:350px" id="single-hero"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-70 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignwide"><!-- wp:separator {"className":"is-style-short"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-short"/>
+<!-- /wp:separator -->
+
+<!-- wp:post-title {"textAlign":"left","level":1} /-->
+
+<!-- wp:post-date /--></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
